51b3d4103baf0ec052211c51294ee9d6615b8595
webopenfathers <1290443925@qq.com>
2023-04-02 11:20:03 +0800

    2023 4-2 11:12 项目创建和环境搭建

---
+++ pages/index/index.vue
@@ -0,0 +1,26 @@
+<template>
+	<view class="container">
+	hello
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				href: 'https://uniapp.dcloud.io/component/README?id=uniui'
+			}
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.container {
+		padding: 20px;
+		font-size: 14px;
+		line-height: 24px;
+	}
+</style>
