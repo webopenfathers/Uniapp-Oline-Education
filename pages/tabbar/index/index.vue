776297f6ba47cc6d23ea11d1d9e74c554ac20c44
webopenfathers <1290443925@qq.com>
2023-04-02 13:46:46 +0800

    2023 4-2 13:46 首页轮播图组件开发

--- pages/tabbar/index/index.vue
+++ pages/tabbar/index/index.vue
@@ -1,6 +1,12 @@
 <template>
 	<view>
 		<f-search-bar></f-search-bar>
+		<swiper style="310rpx" :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000">
+			<swiper-item class="flex justify-center" v-for="(item,index) in swiper" :key="indxe">
+				<image style="width: 720rpx;height: 300rpx;" class="rounded shadow" :src="item.src"
+					mode="aspectFill"></image>
+			</swiper-item>
+		</swiper>
 	</view>
 </template>
 
@@ -8,7 +14,14 @@
 	export default {
 		data() {
 			return {
-
+				swiper: [
+					{
+						src:'/static/demo/banner/banner1.png'
+					},
+					{
+						src:'/static/demo/banner/banner2.png'
+					}
+				]
 			}
 		},
 		methods: {
