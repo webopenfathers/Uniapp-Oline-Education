fd271005bc3fb76eaee6084e33dc50733be173e3
webopenfathers <1290443925@qq.com>
2023-04-02 14:29:39 +0800

    2023 4-2 14:29 优惠券组件开发

---
+++ components/coupon-list/coupon-list.vue
@@ -0,0 +1,32 @@
+<template>
+	<view class="p-2">
+		<scroll-view scroll-x="true" class="scroll-row">
+			<view style="width: 310rpx;display: inline-flex;color: #fff;" class="mr-3" v-for="i in 5" :key="i">
+				<view class="flex flex-column align-center justify-center bg-hover-warning px-3 py-2"
+					style="border-right: 4rpx dashed;">
+					<text>￥100</text>
+					<text class="font">满￥200可用</text>
+				</view>
+				<view style="120rpx" hover-class="bg-hover-warning"
+					class="flex align-center justify-center bg-warning font px-2">
+					领取
+				</view>
+			</view>
+		</scroll-view>
+	</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		name: "coupon-list",
+		data() {
+			return {
+			};
+		}
+	}
+</script>
+
+<style>
+
+</style>
--- pages/tabbar/index/index.vue
+++ pages/tabbar/index/index.vue
@@ -2,12 +2,15 @@
 	<view>
 		<f-search-bar></f-search-bar>
 		<swiper style="310rpx" :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000">
-			<swiper-item class="flex justify-center" v-for="(item,index) in swiper" :key="indxe">
-				<image style="width: 720rpx;height: 300rpx;" class="rounded shadow" :src="item.src"
-					mode="aspectFill"></image>
+			<swiper-item class="flex justify-center" v-for="(item,index) in swiper" :key="index">
+				<image style="width: 720rpx;height: 300rpx;" class="rounded shadow" :src="item.src" mode="aspectFill">
+				</image>
 			</swiper-item>
 		</swiper>
 		<icon-nav :list="iconNav"></icon-nav>
+		<coupon-list></coupon-list>
+		
+		
 	</view>
 </template>
 
@@ -15,46 +18,44 @@
 	export default {
 		data() {
 			return {
-				swiper: [
-					{
-						src:'/static/demo/banner/banner1.png'
+				swiper: [{
+						src: '/static/demo/banner/banner1.png'
 					},
 					{
-						src:'/static/demo/banner/banner2.png'
+						src: '/static/demo/banner/banner2.png'
 					}
 				],
-				iconNav:[
-					{
-						name:'活动',
-						src:"/static/demo/icon/hd.png"
+				iconNav: [{
+						name: '活动',
+						src: "/static/demo/icon/hd.png"
 					},
 					{
-						name:'考试',
-						src:"/static/demo/icon/test.png"
+						name: '考试',
+						src: "/static/demo/icon/test.png"
 					},
 					{
-						name:'秒杀',
-						src:"/static/demo/icon/ms.png"
+						name: '秒杀',
+						src: "/static/demo/icon/ms.png"
 					},
 					{
-						name:'拼团',
-						src:"/static/demo/icon/pt.png"
+						name: '拼团',
+						src: "/static/demo/icon/pt.png"
 					},
 					{
-						name:'直播',
-						src:"/static/demo/icon/course.png"
+						name: '直播',
+						src: "/static/demo/icon/course.png"
 					},
 					{
-						name:'专栏',
-						src:"/static/demo/icon/column.png"
+						name: '专栏',
+						src: "/static/demo/icon/column.png"
 					},
 					{
-						name:'电子书',
-						src:"/static/demo/icon/book.png"
+						name: '电子书',
+						src: "/static/demo/icon/book.png"
 					},
 					{
-						name:'社区',
-						src:"/static/demo/icon/ask.png"
+						name: '社区',
+						src: "/static/demo/icon/ask.png"
 					}
 				]
 			}
