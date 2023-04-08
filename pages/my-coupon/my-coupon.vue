6c55d5c79a8cc19496e59668dc69e8aaccf07a87
webopenfathers <1290443925@qq.com>
2023-04-08 17:05:14 +0800

    2023 4-8 17:04 我的优惠券列表开发

--- pages/my-coupon/my-coupon.vue
+++ pages/my-coupon/my-coupon.vue
@@ -1,6 +1,15 @@
 <template>
 	<view>
-		我的优惠券
+		<view class="flex text-white mb-3">
+			<view class="flex flex-column flex-1 bg-hover-warning p-2" style="border-right: 4rpx dashed #fff;">
+				<text class="font-lg">￥50.00</text>
+				<text class="font-sm">适用课程：课程名称</text>
+			</view>
+			<view hover-class="bg-hover-warning" class=" flex justify-center align-center bg-warning"
+				style="width: 200rpx;">
+				立即使用
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -18,5 +27,12 @@
 </script>
 
 <style>
+	page {
+		padding: 30rpx;
+		box-sizing: border-box;
+	}
 
+	.coupon-used>view {
+		background-color: #ccc !important;
+	}
 </style>
