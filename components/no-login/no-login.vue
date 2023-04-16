389ca56bc65ddeaa0f27352ad037bacbb766e6d9
webopenfathers <1290443925@qq.com>
2023-04-16 20:56:06 +0800

    2023 4-16 20:55 学习进度模快开发-未登录时默认显示

---
+++ components/no-login/no-login.vue
@@ -0,0 +1,33 @@
+<template>
+	<view class="onLogin">
+		<image src="/static/noLogin.png" mode="" style="width: 350rpx;height: 350rpx;"></image>
+		<text class="text-muted my-2 font">登陆后才可以查看我的学习哦</text>
+		<button type="default" size="mini" plain @click="navigateTo('/pages/login/login')">立即登录</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		name: "no-login",
+		data() {
+			return {
+
+			};
+		}
+	}
+</script>
+
+<style>
+	.onLogin {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		background-color: #fff;
+		position: fixed;
+		top: 0;
+		left: 0;
+		right: 0;
+		bottom: 0;
+	}
+</style>
--- pages/tabbar/learn/learn.vue
+++ pages/tabbar/learn/learn.vue
@@ -1,18 +1,27 @@
 <template>
 	<view>
 		学习
+		<no-login v-if="!token"></no-login>
 	</view>
 </template>
 
 <script>
+	import {
+		mapState
+	} from 'vuex'
 	export default {
 		data() {
 			return {
-				
+
 			}
 		},
+		computed: {
+			...mapState({
+				token: state => state.token
+			})
+		},
 		methods: {
-			
+
 		}
 	}
 </script>
