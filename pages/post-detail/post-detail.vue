74cb9644b550bda3e6f47d40d220a398f0d5aa21
webopenfathers <1290443925@qq.com>
2023-05-03 19:22:32 +0800

    2023 5-3 19:22 帖子点赞功能

--- pages/post-detail/post-detail.vue
+++ pages/post-detail/post-detail.vue
@@ -36,6 +36,25 @@
 					</view>
 				</view>
 			</view>
+
+
+			<button type="default" class="my-2" size="mini" style="width: 100%;" @click="loadMore"
+				:disabled="loadStatus!=='more'">
+				{{loadStatus==='more'?'点击加载更多':(loadStatus=='noMore'?'没有更多了':'加载中...')}}
+			</button>
+		</view>
+
+
+		<view style="height: 50px;"></view>
+		<!-- 底部操作条 -->
+		<view style="height: 50px;z-index: 1;" class="fixed-bottom bg-white flex align-center px-3">
+			<!-- 输入框样式 -->
+			<view class=" border rounded flex-1 px-2 py-1 text-light-muted bg-light mr-2">说一句吧</view>
+			<!-- 点赞按钮 -->
+			<view class="flex align-center" :class="detail.issupport?'text-danger':''">
+				<text class="iconfont icon-dianzan2" style="font-size: 20px;"></text>
+				<text class="ml-1">{{detail.support_count||'点赞'}}</text>
+			</view>
 		</view>
 	</view>
 </template>
@@ -100,7 +119,16 @@
 			this.getCommentList()
 		},
 		methods: {
+			loadMore() {
+				if (this.loadStatus != 'more') {
+					return
+				}
+
+				this.page = this.page + 1
+				this.getCommentList()
+			},
 			getCommentList() {
+				this.loadStatus = 'loading'
 				this.$api.getPostComments({
 					post_id: this.detail.id,
 					page: this.page
